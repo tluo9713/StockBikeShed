0394bcf71cfc615e2af20a42e30e882850418c53
Thomas Luo <tluo9713@gmail.com>
2019-10-02 16:59:07 -0400

    added transaction routes

--- server/api-routes/index.js
+++ server/api-routes/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 
-//users not implemented
 router.use('/users', require('./users'));
 router.use('/transactions', require('./transactions'));
-//add more routes as needed
 
+//Error handling for if there is an invalid request.
 router.use((req, res, next) => {
   const err = new Error('Not found.');
   err.status = 404;
--- server/api-routes/transactions.js
+++ server/api-routes/transactions.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
-const Transactions = require('../db/models/transaction');
+// const Transaction = require('../db/models/transaction');
+const { User, Transaction } = require('../db/models');
 
 //We want to have routes to be able to grab user info. Such as when they sign in they should be able to see their funds and also edit their information.
 
 //Grab all tranactions, for testing purposes. If planning on keeping this, may need to have some authentication.
 router.get('/', async (req, res, next) => {
   try {
-    const allTransactions = await Transactions.findAll();
+    const allTransactions = await Transaction.findAll();
     res.status(200).json(allTransactions);
   } catch (error) {
     next(error);
@@ -17,7 +18,7 @@ router.get('/', async (req, res, next) => {
 router.get('/:id', async (req, res, next) => {
   const id = req.params.id;
   try {
-    const userTransactions = await Transactions.findAll({
+    const userTransactions = await Transaction.findAll({
       where: { userId: id },
     });
     res.status(200).json(userTransactions);
@@ -52,12 +53,19 @@ router.get('/:id', async (req, res, next) => {
 
 //Creating a new user route. Destructured because we don't want to take any additional input such as being given additional funds when they shouldn't.
 router.post('/', async (req, res, next) => {
-  const { ticker, shares, purchasePrice } = req.body;
+  const { ticker, shares, purchasePrice, userId } = req.body;
+  //WARNING destructuring for user ID but in the future will need to do several things:
+  //check if there are enough funds else fail transaction
+  //grab user id from the request
+
+  console.log('this is user id', typeof userId);
   try {
-    const newTransaction = await Transactions.create({
+    // console.log(await Users.findByPk(userId));
+    const newTransaction = await Transaction.create({
       ticker,
       shares,
       purchasePrice,
+      userId: 1,
     });
     res.status(201).json(newTransaction);
   } catch (error) {
