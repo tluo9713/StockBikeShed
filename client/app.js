08857aaea75bb219e6273ab7817c79807632b7b4
Thomas Luo <tluo9713@gmail.com>
2019-10-06 11:11:29 -0400

    logout works

--- client/app.js
+++ client/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import { ToastContainer, toast } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
-// import { Navbar } from './components';
+import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import Routes from './routes';
 const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+      <NavBar />
       <HomePage />
       <Routes />
       {/* <ToastContainer /> */}
