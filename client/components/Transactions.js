2282e64eb8c3beceaadc26865b0c5de362593f24
Thomas Luo <tluo9713@gmail.com>
2019-10-06 11:54:19 -0400

    transactions is now a dumb component

--- client/components/Transactions.js
+++ client/components/Transactions.js
@@ -1,54 +1,26 @@
 import React from 'react';
 import TransactionStocks from './TransactionStocks';
 import { connect } from 'react-redux';
-import { getUserTransaction } from '../store/transaction';
 
-class Transactions extends React.Component {
-  constructor() {
-    super();
+const Transactions = props => {
+  let { user } = props;
+  let transactions = [];
+  if (user.id) {
+    transactions = props.transaction.transactionHistory.reverse();
   }
-  //grab user info for stocks
-  componentDidMount() {
-    console.log('console', this.props.user);
-    this.props.grabTransactions(this.props.user);
-  }
-  render() {
-    let { user } = this.props;
-    let transactions = [];
-    if (user.id) {
-      transactions = this.props.transaction.transactionHistory.reverse();
-    }
-    // let transactions = this.props.transaction.transactionHistory;
-    // if (transactions) {
-    //   transactions.reverse();
-    // }
-    // if (transactions)
-    // console.log('transactions', test);
-    // console.log(transactions);
-    return (
-      <div>
-        {transactions.map((transaction, index) => (
-          <TransactionStocks key={index} transaction={transaction} />
-        ))}
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      {transactions.map((transaction, index) => (
+        <TransactionStocks key={index} transaction={transaction} />
+      ))}
+    </div>
+  );
+};
 
 const mapState = state => ({
   user: state.user,
   transaction: state.transaction || {},
 });
 
-const mapDispatch = dispatch => {
-  return {
-    grabTransactions(user) {
-      dispatch(getUserTransaction(user.id));
-    },
-  };
-};
-
-export default connect(
-  mapState,
-  mapDispatch
-)(Transactions);
+export default connect(mapState)(Transactions);
--- public/bundle.js
+++ public/bundle.js
@@ -676,76 +676,25 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var _TransactionStocks__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./TransactionStocks */ "./client/components/TransactionStocks.js");
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-/* harmony import */ var _store_transaction__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../store/transaction */ "./client/store/transaction.js");
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
 
 
+var Transactions = function Transactions(props) {
+  var user = props.user;
+  var transactions = [];
 
+  if (user.id) {
+    transactions = props.transaction.transactionHistory.reverse();
+  }
 
-var Transactions =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(Transactions, _React$Component);
-
-  function Transactions() {
-    _classCallCheck(this, Transactions);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(Transactions).call(this));
-  } //grab user info for stocks
-
-
-  _createClass(Transactions, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      console.log('console', this.props.user);
-      this.props.grabTransactions(this.props.user);
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var user = this.props.user;
-      var transactions = [];
-
-      if (user.id) {
-        transactions = this.props.transaction.transactionHistory.reverse();
-      } // let transactions = this.props.transaction.transactionHistory;
-      // if (transactions) {
-      //   transactions.reverse();
-      // }
-      // if (transactions)
-      // console.log('transactions', test);
-      // console.log(transactions);
-
-
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, transactions.map(function (transaction, index) {
-        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_TransactionStocks__WEBPACK_IMPORTED_MODULE_1__["default"], {
-          key: index,
-          transaction: transaction
-        });
-      }));
-    }
-  }]);
-
-  return Transactions;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, transactions.map(function (transaction, index) {
+    return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_TransactionStocks__WEBPACK_IMPORTED_MODULE_1__["default"], {
+      key: index,
+      transaction: transaction
+    });
+  }));
+};
 
 var mapState = function mapState(state) {
   return {
@@ -754,15 +703,7 @@ var mapState = function mapState(state) {
   };
 };
 
-var mapDispatch = function mapDispatch(dispatch) {
-  return {
-    grabTransactions: function grabTransactions(user) {
-      dispatch(Object(_store_transaction__WEBPACK_IMPORTED_MODULE_3__["getUserTransaction"])(user.id));
-    }
-  };
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_2__["connect"])(mapState, mapDispatch)(Transactions));
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_2__["connect"])(mapState)(Transactions));
 
 /***/ }),
 
