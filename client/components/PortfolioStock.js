56a04c6253fc943713046d60b95ad466cc238cf0
Thomas Luo <tluo9713@gmail.com>
2019-10-07 06:05:44 -0400

    grabs current data to display for portfolio;

--- client/components/PortfolioStock.js
+++ client/components/PortfolioStock.js
@@ -1,14 +1,66 @@
 import React from 'react';
+import axios from 'axios';
 
-const PortfolioStock = props => {
-  let { name, amount } = props;
+class PortfolioStock extends React.Component {
+  constructor() {
+    super();
+    this.state = { loaded: false };
+  }
+  async componentDidMount() {
+    await this.grabCurrentData();
+  }
+  async grabCurrentData() {
+    const { name } = this.props;
+    const url =
+      'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=';
+    let topSecretApiKey = 'YIEAB87E08BESE7W';
+    try {
+      let res = await axios.get(`${url}${name}&apikey=${topSecretApiKey}`);
+      console.log('portfolio,', res.data['Global Quote']);
+      const currentPrice = res.data['Global Quote']['05. price'];
+      const openPrice = res.data['Global Quote']['02. open'];
+      console.log('portfolio,1', currentPrice, openPrice);
+      let stockPerformance;
+      if (currentPrice === openPrice) {
+        stockPerformance = 'No Change';
+      } else if (currentPrice > openPrice) {
+        stockPerformance = 'Net Gain';
+      } else {
+        stockPerformance = 'Net Loss';
+      }
+      this.setState({
+        stock: currentPrice,
+        status: stockPerformance,
+        loaded: true,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  return (
-    <div>
-      <h1>Name: {name}</h1>
-      <h2>amount: {amount}</h2>
-    </div>
-  );
-};
+  render() {
+    let { name, amount } = this.props;
+
+    return (
+      <div>
+        <h1>Name: {name}</h1>
+        <h2>amount: {amount}</h2>
+        {this.state.loaded ? (
+          <div>
+            <h1>Current: {this.state.stock}</h1>
+            <h1>Evaluation: {this.state.stock * amount}</h1>
+            <h1>{this.state.status}</h1>
+          </div>
+        ) : (
+          <h1>
+            Loading (We are using a free API and are throttled by the amount of
+            calls we can make, please refresh data again shortly
+          </h1>
+        )}
+        <button onClick={this.grabCurrentData}>Refresh</button>
+      </div>
+    );
+  }
+}
 
 export default PortfolioStock;
--- public/bundle.js
+++ public/bundle.js
@@ -410,13 +410,151 @@ var mapState = function mapState(state) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
-var PortfolioStock = function PortfolioStock(props) {
-  var name = props.name,
-      amount = props.amount;
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Name: ", name), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "amount: ", amount));
-};
+
+
+var PortfolioStock =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(PortfolioStock, _React$Component);
+
+  function PortfolioStock() {
+    var _this;
+
+    _classCallCheck(this, PortfolioStock);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(PortfolioStock).call(this));
+    _this.state = {
+      loaded: false
+    };
+    return _this;
+  }
+
+  _createClass(PortfolioStock, [{
+    key: "componentDidMount",
+    value: function () {
+      var _componentDidMount = _asyncToGenerator(
+      /*#__PURE__*/
+      regeneratorRuntime.mark(function _callee() {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return this.grabCurrentData();
+
+              case 2:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function componentDidMount() {
+        return _componentDidMount.apply(this, arguments);
+      }
+
+      return componentDidMount;
+    }()
+  }, {
+    key: "grabCurrentData",
+    value: function () {
+      var _grabCurrentData = _asyncToGenerator(
+      /*#__PURE__*/
+      regeneratorRuntime.mark(function _callee2() {
+        var name, url, topSecretApiKey, res, currentPrice, openPrice, stockPerformance;
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                name = this.props.name;
+                url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=';
+                topSecretApiKey = 'YIEAB87E08BESE7W';
+                _context2.prev = 3;
+                _context2.next = 6;
+                return axios__WEBPACK_IMPORTED_MODULE_1___default.a.get("".concat(url).concat(name, "&apikey=").concat(topSecretApiKey));
+
+              case 6:
+                res = _context2.sent;
+                console.log('portfolio,', res.data['Global Quote']);
+                currentPrice = res.data['Global Quote']['05. price'];
+                openPrice = res.data['Global Quote']['02. open'];
+                console.log('portfolio,1', currentPrice, openPrice);
+
+                if (currentPrice === openPrice) {
+                  stockPerformance = 'No Change';
+                } else if (currentPrice > openPrice) {
+                  stockPerformance = 'Net Gain';
+                } else {
+                  stockPerformance = 'Net Loss';
+                }
+
+                this.setState({
+                  stock: currentPrice,
+                  status: stockPerformance,
+                  loaded: true
+                });
+                _context2.next = 18;
+                break;
+
+              case 15:
+                _context2.prev = 15;
+                _context2.t0 = _context2["catch"](3);
+                console.error(_context2.t0);
+
+              case 18:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2, this, [[3, 15]]);
+      }));
+
+      function grabCurrentData() {
+        return _grabCurrentData.apply(this, arguments);
+      }
+
+      return grabCurrentData;
+    }()
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          name = _this$props.name,
+          amount = _this$props.amount;
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Name: ", name), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "amount: ", amount), this.state.loaded ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Current: ", this.state.stock), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Evaluation: ", this.state.stock * amount), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, this.state.status)) : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Loading (We are using a free API and are throttled by the amount of calls we can make, please refresh data again shortly"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        onClick: this.grabCurrentData
+      }, "Refresh"));
+    }
+  }]);
+
+  return PortfolioStock;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
 
 /* harmony default export */ __webpack_exports__["default"] = (PortfolioStock);
 
@@ -1153,7 +1291,7 @@ var updateUserFunds = function updateUserFunds(funds) {
 /*!*******************************!*\
   !*** ./client/store/index.js ***!
   \*******************************/
-/*! exports provided: default, getUserTransaction, createNewTransaction, me, signUpUser, auth, logout */
+/*! exports provided: default, me, signUpUser, auth, logout, getUserTransaction, createNewTransaction */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
